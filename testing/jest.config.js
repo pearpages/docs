109bafe2040f8bdf83cb45c1528eea526196e0ad
Pere Pages <pere@soms.cat>
2019-03-24 20:11:16 +0100

    Update jest.config.js

--- testing/jest.config.js
+++ testing/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'shared'],
   moduleNameMapper: {
     '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js')
